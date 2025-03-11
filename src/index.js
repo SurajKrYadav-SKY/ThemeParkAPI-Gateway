9418ddb1165bc3ed955c5bac90d6e56096ca2785
Suraj Kumar Yadav <suraj.mails99@gmail.com>
2025-03-11 14:47:34 +0530

    setup the request logger using morgan package

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 const bodyParser = require("body-parser");
 const express = require("express");
 const { PORT } = require("./config/serverConfig");
+const morgan = require("morgan");
 
 const setupAndStartServer = async () => {
   const app = express();
 
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
+
+  app.use(morgan("combined"));
+
+  app.get("/api/home", (req, res) => {
+    return res.json({
+      messsgage: "OK",
+    });
+  });
 
   app.listen(PORT, () => {
     console.log("Server started at port:", PORT);
